20418f207c6ac632c18c07503b5cdd2d8cbd61ce
Hektor-alfatih <160695682+Hektor-alfatih@users.noreply.github.com>
2024-10-03 11:05:21 +0700

    update

---
+++ exam.mjs
@@ -0,0 +1,11 @@
+export  for (let i = 1; i <= 20; i++) {
+
+    if (i % 4 === 0) {
+        console.log("Bilangan genap bisa dibagi 4", i);
+    } else if (i % 2 === 0) {
+        console.log("Bilangan genap tidak bisa dibagi 4", i);
+    } else if  (i % 2 === 1) {
+        console.log("Bilangan ganjil", i);
+    }
+
+    } 
